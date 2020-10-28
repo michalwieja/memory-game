a00e9ee2c2ff4404f3ec2a515e8269d3940b3aa5
mwConceptX <mwieja@yahoo.com>
2020-10-28 16:58:06 +0100

    2nd

--- app.js
+++ app.js
@@ -11,21 +11,21 @@ const cardsArray = [
   { name: "kreweta", img: "./img/kreweta.svg" },
   { name: "muszla", img: "./img/muszla.svg" },
   { name: "muszla", img: "./img/muszla.svg" },
-  { name: "osmiornica", img: "./img/osmiornica.svg" },
-  { name: "osmiornica", img: "./img/osmiornica.svg" },
-  { name: "plaszcz", img: "./img/plaszcz.svg" },
-  { name: "plaszcz", img: "./img/plaszcz.svg" },
-  { name: "rekin", img: "./img/rekin.svg" },
-  { name: "rekin", img: "./img/rekin.svg" },
-  { name: "ryba", img: "./img/ryba.svg" },
-  { name: "ryba", img: "./img/ryba.svg" },
-  { name: "skalar", img: "./img/skalar.svg" },
-  { name: "skalar", img: "./img/skalar.svg" },
-  { name: "zolw", img: "./img/zolw.svg" },
-  { name: "zolw", img: "./img/zolw.svg" },
+  // { name: "osmiornica", img: "./img/osmiornica.svg" },
+  // { name: "osmiornica", img: "./img/osmiornica.svg" },
+  // { name: "plaszcz", img: "./img/plaszcz.svg" },
+  // { name: "plaszcz", img: "./img/plaszcz.svg" },
+  // { name: "rekin", img: "./img/rekin.svg" },
+  // { name: "rekin", img: "./img/rekin.svg" },
+  // { name: "ryba", img: "./img/ryba.svg" },
+  // { name: "ryba", img: "./img/ryba.svg" },
+  // { name: "skalar", img: "./img/skalar.svg" },
+  // { name: "skalar", img: "./img/skalar.svg" },
+  // { name: "zolw", img: "./img/zolw.svg" },
+  // { name: "zolw", img: "./img/zolw.svg" },
 ];
 
-// cardsArray.sort(() => 0.5 - Math.random());
+cardsArray.sort(() => 0.5 - Math.random());
 
 let choosenCards = [];
 let choosenCardsId = [];
@@ -35,30 +35,50 @@ const grid = document.querySelector(".grid");
 const score = document.querySelector("#score");
 
 cardsArray.forEach((card, index) => {
-  let img = document.createElement("img");
-  img.setAttribute("src", "./img/gili.svg");
-  img.setAttribute("data-id", index);
-  img.addEventListener("click", flipcard);
-  grid.appendChild(img);
+  let button = document.createElement("button");
+
+  button.setAttribute("data-id", index);
+  button.addEventListener("click", flipcard);
+  grid.appendChild(button);
 });
 
 // check
 
+const match = document.querySelector(".match");
+const fail = document.querySelector(".fail");
+const won = document.querySelector(".won");
+
 function check() {
-  let cards = document.querySelectorAll("img");
+  let cards = document.querySelectorAll("button");
   let firstId = choosenCardsId[0];
   let secondId = choosenCardsId[1];
 
   if (choosenCards[0] === choosenCards[1]) {
-    alert("ok");
-    cards[firstId].setAttribute("src", "./img/white.svg");
-    cards[secondId].setAttribute("src", "./img/white.svg");
+    match.style.opacity = 1;
+    match.style.visibility = "visible";
+
+    setTimeout(function () {
+      match.style.opacity = 0;
+      match.style.visibility = "hidden";
+    }, 1000);
+
+    cards[firstId].style.backgroundImage = `url(./img/white.svg)`;
+    cards[secondId].style.backgroundImage = `url(./img/white.svg)`;
 
     cardsWon.push(choosenCards);
   } else {
-    alert("nie tym razem");
-    cards[firstId].setAttribute("src", "./img/gili.svg");
-    cards[secondId].setAttribute("src", "./img/gili.svg");
+    fail.style.opacity = 1;
+    fail.style.visibility = "visible";
+
+    setTimeout(function () {
+      fail.style.opacity = 0;
+      fail.style.visibility = "hidden";
+    }, 1000);
+
+    cards[firstId].style.backgroundImage = `url(./img/gili.svg)`;
+    cards[secondId].style.backgroundImage = `url(./img/gili.svg)`;
+    cards[firstId].disabled = false;
+    cards[secondId].disabled = false;
   }
 
   choosenCards = [];
@@ -66,7 +86,8 @@ function check() {
 
   score.textContent = cardsWon.length;
   if (cardsWon.length === cardsArray.length / 2) {
-    alert("wygrales");
+    won.style.opacity = 1;
+    won.style.visibility = "visible";
   }
 }
 
@@ -74,7 +95,8 @@ function check() {
 
 function flipcard() {
   let id = this.getAttribute("data-id");
-  this.setAttribute("src", cardsArray[id].img);
+  this.style.backgroundImage = `url(${cardsArray[id].img})`;
+  this.disabled = true;
 
   choosenCards.push(cardsArray[id].name);
   choosenCardsId.push(id);
